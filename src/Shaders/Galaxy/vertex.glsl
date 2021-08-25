618b1e37ed6664044ca8de575725f42f359e88cd
Erez Yosepov <yosepov.erez@gmail.com>
2021-08-25 14:28:35 +0300

    Add Galaxy project to github

---
+++ src/Shaders/Galaxy/vertex.glsl
@@ -0,0 +1,38 @@
+
+
+uniform float uTime;
+uniform float uSize;
+
+attribute float aScale;
+attribute vec3 aRandomness;
+
+varying vec3 vColor;
+
+ void main()
+        {
+            vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+            // Spin
+            float angle = atan(modelPosition.x , modelPosition.z);
+            float distanceToCenter = length(modelPosition.xz);
+            float angleOffSet = (1.0 / distanceToCenter) * uTime * 0.2;
+            angle += angleOffSet;
+            modelPosition.x = cos(angle) * distanceToCenter;
+            modelPosition.z = sin(angle) * distanceToCenter;
+
+            // Randomness
+            modelPosition.xyz += aRandomness;
+            modelPosition.x += aRandomness.x;
+            modelPosition.y += aRandomness.y;
+            modelPosition.z += aRandomness.z;
+
+
+            vec4 viewPosition = viewMatrix * modelPosition;
+            vec4 projectedPosition = projectionMatrix * viewPosition;
+
+            gl_Position = projectedPosition;
+            gl_PointSize = uSize * aScale;
+            gl_PointSize *= (1.0 / - viewPosition.z); 
+            
+            vColor = color;
+        }
